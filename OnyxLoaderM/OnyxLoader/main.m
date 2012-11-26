fc68929a61183306cb675eb6233eb1d73380b918
Nava Whiteford <new@sgenomics.org>
2012-11-26 04:52:25 +0000

    onyxloader mac

---
+++ OnyxLoaderM/OnyxLoader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  OnyxLoader
+//
+//  Created by new on 05/11/2012.
+//  Copyright (c) 2012 SGenomics Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
---
+++ OnyxLoaderM/OnyxLoaderTests/OnyxLoaderTests.h
@@ -0,0 +1,13 @@
+//
+//  OnyxLoaderTests.h
+//  OnyxLoaderTests
+//
+//  Created by new on 05/11/2012.
+//  Copyright (c) 2012 SGenomics Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OnyxLoaderTests : SenTestCase
+
+@end
