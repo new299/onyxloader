ef03bc07bebae52b641558647bc46712cf784dd2
Nava Whiteford <new@sgenomics.org>
2013-03-18 00:35:01 +0000

    Added experimental firmware loader, ability to download log as CSV, removed log upload button

--- OnyxLoaderMac/OnyxLoader/AppDelegate.mm
+++ OnyxLoaderMac/OnyxLoader/AppDelegate.mm
@@ -16,9 +16,60 @@
     // Insert code here to initialize your application
 }
 
-- (IBAction)UpdateFirmware:(NSButton *)sender {
-    NSLog(@"Update Firmware");
+- (IBAction)SaveCSV:(id)sender {
+
+    NSLog(@"SaveCSV");
+    NSSavePanel * savePanel = [NSSavePanel savePanel];
+
+    [savePanel setAllowedFileTypes:[NSArray arrayWithObject:@"csv"]];
+
+    [savePanel beginWithCompletionHandler:^(NSInteger result){
+        if (result == NSFileHandlingPanelOKButton) {
+            NSLog(@"Got URL: %@", [savePanel URL]);
+            
+            char *data = do_get_log_csv();
     
+            NSString * str = [NSString stringWithFormat:@"%s", data];
+            free(data);
+            
+            [str writeToURL:[savePanel URL] atomically:YES encoding:NSUTF8StringEncoding error:NULL];
+        }
+    }];
+
+}
+
+- (IBAction)SetTime:(id)sender {
+  NSLog(@"Set Time");
+}
+
+
+- (IBAction)UpdateExperimental:(id)sender {
+    NSLog(@"Update Firmware - experimental");
+    
+    // Download flash image from http://41j.com/safecast_exp.bin
+    // Determile cache file path
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *filePath = [NSString stringWithFormat:@"%@/%@", [paths objectAtIndex:0],@"index.html"];
+    
+    // Download and write to file
+    NSURL *url = [NSURL URLWithString:@"http://41j.com/safecast_exp.bin"];
+    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    [urlData writeToFile:filePath atomically:YES];
+
+    int argc=3;
+    char *argv[3];
+    char *argv0 = "flash";
+    char *argv1 = "-f";
+    char *argv2 = (char *) filePath.UTF8String;
+    argv[0] = argv0;
+    argv[1] = argv1;
+    argv[2] = argv2;
+    
+    int result = do_flash_main(argc,argv);
+}
+
+- (IBAction)UpdateFirmware:(NSButton *)sender {
+    NSLog(@"Update Firmware - stable");
     
     // Download flash image from http://41j.com/safecast_latest.bin
     // Determile cache file path
@@ -30,8 +81,6 @@
     NSData *urlData = [NSData dataWithContentsOfURL:url];
     [urlData writeToFile:filePath atomically:YES];
 
-    
-    
     int argc=3;
     char *argv[3];
     char *argv0 = "flash";
@@ -49,7 +98,6 @@
     
     char *data = do_get_log();
     
-//    NSString* str = @"teststring";
     NSString * str = [NSString stringWithFormat:@"%s", data];
     free(data);
     
